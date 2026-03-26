8629df316e073392805093c2bb4e29df484f67aa
NABIRIY <mail@nodnd.ru>
2026-03-26 15:43:07 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,4 @@
-﻿#include <iostream>
-#include <string>
-#include <thread>
-
-#ifdef _WIN32
-#include <winsock2.h>
-#pragma comment(lib, "ws2_32.lib")
-#define CLOSESOCKET closesocket
-#else
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#define SOCKET int
-#define INVALID_SOCKET -1
-#define SOCKET_ERROR -1
-#define CLOSESOCKET close
-#endif
+﻿#include "main.h"
 
 void receiveMessages(SOCKET sock) {
     char buffer[1024];
--- main.h
+++ main.h
@@ -4,5 +4,22 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <thread>
 
+#ifdef _WIN32
+	#include <winsock2.h>
+	#pragma comment(lib, "ws2_32.lib")
+	#define CLOSESOCKET closesocket
+#else
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <unistd.h>
+	#define SOCKET int
+	#define INVALID_SOCKET -1
+	#define SOCKET_ERROR -1
+	#define CLOSESOCKET close
+#endif
 // TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
